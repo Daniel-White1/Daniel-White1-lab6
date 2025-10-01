5b67642a067372643875b355eb8d26dfc1471219
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-10-01 15:13:26 -0400

    Started work on thing list and created Node file instead of making it a private object inside thing list

---
+++ Node.java
@@ -0,0 +1,10 @@
+public class Node {
+
+    private Thing data;
+    private Node next;
+    
+    public Node(Thing t) {
+        this.data = t;
+        this.next = null;
+    }
+}
--- ThingList.java
+++ ThingList.java
@@ -1,3 +1,17 @@
 public class ThingList {
+
+    //Thing List creates a head node and sets it to null
+    Node head;
+
+    //Constructor for an empty ThingList sets the head to null
+    public ThingList(){
+        head = null;
+    }
+
+    //The constructor for a ThingList that includes a head node sets the head to a node
+    public ThingList(Node node){
+        head = node;
+    }
     
+
 }
