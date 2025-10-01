67d1d2701fac98646aacb47fad783f475f2fedbc
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-10-01 16:16:14 -0400

    worked on thinglist

--- ThingList.java
+++ ThingList.java
@@ -12,6 +12,22 @@ public class ThingList {
     public ThingList(Node node){
         head = node;
     }
+   
+    //This code just relies on inserting each node at the end of the link so we dont have to rely on inserting the middle
+    // just the head and end
     
+    private void insert(Node node){
+        if(this.head == null){
+            this.head = node;
+        }
 
+        Node currentNode = this.head;
+        if(this.head != null){
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+
+    }
 }
